3b7bd62b9cc922cbf43b87383cff36287bb43f43
Cristian-Tavarez <cristian22041@gmail.com>
2026-06-27 07:33:25 -0700

    Agrega el apartado de imagen en model/Planet.kt

--- app/src/main/java/com/example/apidragonballapp/domain/model/Planet.kt
+++ app/src/main/java/com/example/apidragonballapp/domain/model/Planet.kt
@@ -3,5 +3,6 @@ package com.example.apidragonballapp.domain.model
 data class Planet(
     val id: Int,
     val name: String,
-    val description: String
+    val description: String,
+    val image: String
 )
