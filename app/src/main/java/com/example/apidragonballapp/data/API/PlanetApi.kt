14c9bf1e3f3dd8e9bc4ffedcca092f5621dd51e0
Cristian-Tavarez <cristian22041@gmail.com>
2026-06-27 08:18:06 -0700

    Modifica el PlanetApi.kt y Agrega Characters

--- app/src/main/java/com/example/apidragonballapp/data/API/PlanetApi.kt
+++ app/src/main/java/com/example/apidragonballapp/data/API/PlanetApi.kt
@@ -16,8 +16,28 @@ data class SimplePlanetDto(
     val description: String,
     val image: String
 )
+@JsonClass(generateAdapter = true)
+data class CharacterResponse(
+    @Json(name = "items") val items: List<SimpleCharacterDto>
+)
+
+    @JsonClass(generateAdapter = true)
+data class SimpleCharacterDto(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val maxKi: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val affiliation: String
+)
 
 interface PlanetApi {
     @GET("planets")
     suspend fun getPlanets(): PlanetResponse
+
+    @GET("characters")
+    suspend fun getCharacters(): CharacterResponse
 }
